28e803c56ca2a8c4158b8e8591a1e1c88bed0a06
Valentyn <valentunpron@gmail.com>
2022-10-13 13:22:22 +0300

    complete burger menu

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', () => {
+	//burger 
+	document.querySelector('.slider__burger').addEventListener('click', e => {
+		document.querySelector('.header__nav').classList.add('burger-active');
+		document.querySelector('.slider__burger').classList.add('burger-active');
+		document.querySelector('body').classList.add('lock');
+	});
+	document.querySelector('.header__return').addEventListener('click', e => {
+		document.querySelector('.header__nav').classList.remove('burger-active');
+		document.querySelector('.slider__burger').classList.remove('burger-active');
+		document.querySelector('body').classList.remove('lock');
+	})
+	console.log('dsd')
+});
